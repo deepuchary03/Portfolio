3dd1bfd8aee5305469a5963a9129d87170a05e50
deepuchary03 <deepuchary03@gmail.com>
2024-11-05 13:32:17 +0530

    about update

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's S Pranav, but you can also call me Deepu Chary.",
+    description1: "Hello I'm  S Pranav, also known as Deepu Chary.",
     description2: "I'm a dedicated student studying CS at Teegala Krishna Reddy Engg College. My passion for coding, design drives me to explore innovative solutions and tackle challenges head-on.",
     image: 2
 
