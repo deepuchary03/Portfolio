e1ed6f33253e997f088771181f5e803a6077153e
deepuchary03 <deepuchary03@gmail.com>
2025-04-03 16:52:49 +0530

    project 2

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,10 +2,11 @@
 //import two from "../assets/svg/projects/two.svg";
 import three from "../assets/svg/projects/three.svg";
 //import four from "../assets/svg/projects/four.svg";
-//import five from "../assets/svg/projects/five.svg";
+import five from "../assets/svg/projects/five.svg";
 //import six from "../assets/svg/projects/six.svg";
 //import seven from "../assets/svg/projects/seven.svg";
 //import eight from "../assets/svg/projects/eight.svg";
+//import nine from "../assets/svg/projects/m.svg";
 
 export const projectsData = [
   {
@@ -18,17 +19,19 @@ export const projectsData = [
     demo: "https://deepuchary.vercel.app/",
     image: three,
   },
-  /*,
-    {
-        id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
-    },
-    {
+
+  {
+    id: 2,
+    projectName: "Food Delivery App",
+    projectDesc:
+      "This project is a food delivery application that allows users to order food from their favorite restaurants.",
+
+    tags: ["React", "Node js", "MongoDB"],
+    code: "https://github.com/deepuchary03/Food-Delivery-App.git",
+    demo: "https://deepuchary-fd.vercel.app/",
+    image: five,
+  },
+  /* {
         id: 3,
         projectName: 'Weather App',
         projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
