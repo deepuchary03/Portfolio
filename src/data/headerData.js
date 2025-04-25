b436777ba8114a0cb44b86c367a163425eed49ad
deepuchary03 <deepuchary03@gmail.com>
2025-04-25 19:32:11 +0530

    reume update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,11 @@
-import resume from '../assets/pdf/resume.pdf'
-import myprofileimg from '../assets/png/deepu_profile.png'
+import resume from "../assets/pdf/SPranav_Resume.pdf";
+import myprofileimg from "../assets/png/deepu_profile.png";
 
 export const headerData = {
-    name: 'S Pranav',
-    title: "Web Developer",
-    desciption:"Also known as Deepu Chary. As a curious and ambitious student, I'm excited to explore the vast opportunities in my field of interest. With a passion for learning and growth, I'm constantly seeking new experiences and challenges that can helpme develop my skills and knowledge.",
-    image: myprofileimg,
-    resumePdf: resume
-}
+  name: "S Pranav",
+  title: "Web Developer",
+  desciption:
+    "Also known as Deepu Chary. As a curious and ambitious student, I'm excited to explore the vast opportunities in my field of interest. With a passion for learning and growth, I'm constantly seeking new experiences and challenges that can helpme develop my skills and knowledge.",
+  image: myprofileimg,
+  resumePdf: resume,
+};
