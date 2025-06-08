604c97fe459a4f97a102e860fcdae329934abf0d
deepuchary03 <deepuchary03@gmail.com>
2025-06-08 12:19:51 +0530

    update

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,20 +1,23 @@
- export const skillsData = [
-    'HTML',
-    'java',
-    'python',
-    'Javascript',
-    'git',
-    'mysql',
-    'CSS',
-    
-    'Figma',
-    'React',
-    'Bootstrap',
-]
+export const skillsData = [
+  "HTML",
+  "java",
+  "python",
+  "Javascript",
+  "git",
+  "mysql",
+  "CSS",
+  "PostgreSQL",
+  "AWS",
+  "Microsoft Office",
+  "MongoDB",
+  "Canva",
+  "Figma",
+  "React",
+  "Bootstrap",
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
